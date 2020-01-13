b823ebb047ab3296327f8991ee56a6cb011d8a08
unknown <541831011@qq.com>
2020-01-13 17:45:02 +0800

    换了个小的视频

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -9,8 +9,8 @@ $factory->define(Video::class, function (Faker $faker) {
     return [
         'title'=>$faker->sentence,
         'description'=>$faker->text,
-        'video_path'=>env('APP_URL').'/uploads/video/video/201912/27/1755_1577391933.mp4',
-        'video_image'=>env('APP_URL').'\uploads\video\video\201912\27\1755_1577391933.jpg',
+        'video_path'=>env('APP_URL').'/uploads/video/video/202001/12/1320_1578840889.mp4',
+        'video_image'=>env('APP_URL').'/uploads/video/video/202001/12/1320_1578840889.jpg',
         'video_type'=>mt_rand(1,7),
         'user_id'=>mt_rand(1,10),
         'is_reprint'=>mt_rand(0,1),
